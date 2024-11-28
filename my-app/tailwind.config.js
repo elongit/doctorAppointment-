ee3d308d038b86405150165bd6a95efe3db0f7c3
ossama <ossama@gmail.com>
2024-11-28 04:57:53 -0600

    tailwindcss configuaration file

---
+++ my-app/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}", "./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        "primary-color": "#3b82f6",
+        "secondary-color": "#2563eb",
+      },
+      fontFamily: {
+        Inter: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
