fd75da374e5bc0a004377568eec09ff02ce164b7
ossama <ossama@gmail.com>
2024-11-28 04:48:31 -0600

    docotrs card  component

---
+++ my-app/src/components/card.jsx
@@ -0,0 +1,31 @@
+
+import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
+export default function Card({ name, specialty, imageUrl }) {
+    return (
+        <article className="text-[17px] h-fit hover:shadow-lg transition-all p-2 cursor-pointer">
+        <figure className="rounded">
+         <img src={imageUrl} alt={name} className="w-full h-64 object-cover rounded-md"></img>
+        </figure>
+        <div className="p-2">
+            <NavLink className="text-xl text-secondary-color font-semibold">
+                Dr. {name}
+            </NavLink>
+            <p className="text-sm font-bold">{specialty}</p>
+        </div>
+        <div className="flex flex-col gap-1 text-center">
+
+            <NavLink to='/' className="bg-primary-color hover:bg-secondary-color text-white font-bold  py-2 rounded-md">
+                Book an appointment
+            </NavLink>
+        </div>
+    </article>
+
+    );
+}
+
+Card.propTypes = {
+    name : PropTypes.string.isRequired,
+    specialty : PropTypes.string.isRequired,
+    imageUrl : PropTypes.string.isRequired
+}
