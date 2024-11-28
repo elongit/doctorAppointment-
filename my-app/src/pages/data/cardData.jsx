53b48901aceb4a5ecb922f052bb24ac563f88a80
ossama <ossama@gmail.com>
2024-11-28 17:06:35 -0600

    folder contain app data

---
+++ my-app/src/pages/data/cardData.jsx
@@ -0,0 +1,27 @@
+const data = [
+    {
+        id: 1,
+        img: "https://momonahealthcare.com/wp-content/uploads/2024/03/momona_general_consultation.png",
+        Title: "General Consultation",
+        desc: "Book an appointment for a general health check-up with our experienced doctors.",
+    },
+    { 
+        id: 2,
+        img: "https://careresource.org/wp-content/uploads/2023/01/Peds-now-open.jpg",
+        Title: "Pediatrics",
+        desc: "Get expert medical care for your children from our specialized pediatricians.",
+    },
+    {
+        id: 3,
+        img: "https://thumbs.dreamstime.com/b/3d-dentist-smiling-tooth-toothbrush-28185489.jpg",
+        Title: "Dentistry",
+        desc: "Schedule a visit for dental check-ups, cleanings, and other oral health services.",
+    },
+    {
+        id: 4,
+        img: "https://media.istockphoto.com/id/1171292998/es/vector/dermat%C3%B3logo.jpg?s=612x612&w=0&k=20&c=2_A3OwBtBGOrRxT7Lm9Hd4D2iIOD5OhKbZLaL2U8CGo=",
+        Title: "Dermatology",
+        desc: "Consult with our dermatologists for skin-related concerns and treatments.",
+    },
+];
+export default data;
