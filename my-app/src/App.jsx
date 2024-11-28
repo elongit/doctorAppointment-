c1d4f580c83252e32edf169a786b68134fa9ba30
ossama <ossama@gmail.com>
2024-11-28 17:04:34 -0600

    added new routers

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,6 +2,9 @@ import Header from "./components/header"
 import Home from "./pages/home"
 import Login from "./pages/auth/login"
 import SignUp from "./pages/auth/signUp"
+import ForgetPassword from "./pages/auth/forgetPass"
+import EmailCode from "./pages/auth/emailCode"
+import ResetPassword from "./pages/auth/resetPass"
 import Contact from "./pages/contact/contact"
 import Footer from "./components/footer"
 import DoctorList from "./pages/doctorsList"
@@ -20,6 +23,9 @@ function App() {
           <Route path="/signUp" element={<SignUp/>}></Route>
           <Route path="/doctorsList" element={<DoctorList/>}></Route>
           <Route path="*" element={<NotFound/>}></Route>
+          <Route path="/forgetPass" element={<ForgetPassword/>}></Route>
+          <Route path="/emailCode" element={<EmailCode/>}></Route>
+          <Route path="resetPass" element={<ResetPassword/>}></Route>
         </Routes>
         {/* <Footer/> */}
      </BrowserRouter>
