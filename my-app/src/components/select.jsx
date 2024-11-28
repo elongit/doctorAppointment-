579fa0a0a2cb3b134abead594cdc46398d1f56e0
ossama <ossama@gmail.com>
2024-11-28 04:45:04 -0600

    select  component

---
+++ my-app/src/components/select.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+
+
+const Select = ({name , id , options , selectedOption})=>{
+
+    return(
+        <select name={name} id={ id }  className="bg-slate-100 border-2 border-white hover:border-blue-600 p-3 outline-none rounded md:w-full w-fit">
+            <option selected>{selectedOption}</option>
+            {
+                options.map((opt, i)=>
+                    <option key={i}value={opt}>{opt}</option>
+            )
+            }
+        </select>
+    )
+}
+
+Select.propTypes = {
+    name : PropTypes.string.isRequired,
+    id : PropTypes.string.isRequired,
+    options : PropTypes.array.isRequired,
+    selectedOption : PropTypes.string.isRequired
+}
+
+export default Select
