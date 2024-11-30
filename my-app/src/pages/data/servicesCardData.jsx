4397ce7efe226742c2ae063b503255d363d1ff53
ossama <ossama@gmail.com>
2024-11-30 02:30:32 -0600

    added new data folder contain app data

---
+++ my-app/src/pages/data/servicesCardData.jsx
@@ -0,0 +1,52 @@
+const data = [
+    {
+        id: 1,
+        img: "https://momonahealthcare.com/wp-content/uploads/2024/03/momona_general_consultation.png",
+        Title: "General Consultation",
+        desc: "Book an appointment for a general health check-up with our experienced doctors.",
+    },
+    { 
+        id: 2,
+        img: "https://careresource.org/wp-content/uploads/2023/01/Peds-now-open.jpg",
+        Title: "Pediatrics",
+        desc: "Get expert medical care for your children from our specialized pediatricians.",
+    },
+    {
+        id: 3,
+        img: "https://thumbs.dreamstime.com/b/3d-dentist-smiling-tooth-toothbrush-28185489.jpg",
+        Title: "Dentistry",
+        desc: "Schedule a visit for dental check-ups, cleanings, and other oral health services.",
+    },
+    {
+        id: 4,
+        img: "https://media.istockphoto.com/id/1171292998/es/vector/dermat%C3%B3logo.jpg?s=612x612&w=0&k=20&c=2_A3OwBtBGOrRxT7Lm9Hd4D2iIOD5OhKbZLaL2U8CGo=",
+        Title: "Dermatology",
+        desc: "Consult with our dermatologists for skin-related concerns and treatments.",
+    },
+   
+    {
+        id: 5,
+        img: "https://cdn-icons-png.flaticon.com/512/9352/9352168.png",
+        Title: "Optometry",
+        desc: "Get your vision checked and receive professional care from our optometrists.",
+    },
+    {
+        id: 6,
+        img: "https://cdn-icons-png.flaticon.com/512/421/421419.png",
+        Title: "Orthopedics",
+        desc: "Consult with our orthopedic specialists for musculoskeletal health and injuries.",
+    },
+    {
+        id: 7,
+        img: "https://e7.pngegg.com/pngimages/546/916/png-clipart-gynaecology-hysterectomy-hospital-cancer-surgery-gynecology-miscellaneous-purple-thumbnail.png",
+        Title: "Gynecology",
+        desc: "Book an appointment with our gynecologists for women's health care and screenings.",
+    },
+    {
+        id: 8,
+        img: "https://static.thenounproject.com/png/1503536-200.png",
+        Title: "Neurology",
+        desc: "Consult our neurologists for brain and nervous system health and treatments.",
+    }
+];
+export default data;
