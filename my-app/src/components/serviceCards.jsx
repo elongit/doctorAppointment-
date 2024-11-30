5cf407faf64c35cf21b02d90f3e3f779c97a0b04
ossama <ossama@gmail.com>
2024-11-30 02:27:34 -0600

    re styled the service cards

--- my-app/src/components/serviceCards.jsx
+++ my-app/src/components/serviceCards.jsx
@@ -2,12 +2,12 @@ const ServiceCards = ({ data }) => {
   const cards = data.map((d) => (
     <div
       key={d.id}
-      className="font-serif p-6 bg-white hover:shadow-lg rounded py-10 h-fit grid grid-cols-1 place-items-center shadow-inner lg:place-items-start text-center lg:text-start  transition-shadow duration-300"
+      className="font-serif p-6 bg-white hover:shadow-lg flex-shrink-0 basis-80 rounded  h-fit grid grid-cols-1 place-items-center shadow-inner lg:place-items-start text-center lg:text-start  transition-shadow duration-300"
     >
       <figure>
         <img src={d.img} alt={d.Title} className="w-20 rounded" />
       </figure>
-      <h1 className="text-2xl my-4 hover:text-indigo-500">{d.Title}</h1>
+      <h1 className="text-2xl my-4 hover:text-primary-color">{d.Title}</h1>
       <p className="text-[18px] text-gray-700">{d.desc}</p>
     </div>
   ));
@@ -20,7 +20,7 @@ const ServiceCards = ({ data }) => {
         </h1>
       </div>
 
-      <div className="cursor-pointer px-5 lg:px-5  grid-cols-1 md:grid grid-cols-2 lg:grid-cols-4 gap-4 pb-14 mt-10">
+      <div className="cursor-pointer px-5 lg:px-5 flex  overflow-x-auto gap-4 pb-14 mt-10 cardSection">
         {cards}
       </div>
     </>
