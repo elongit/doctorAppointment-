70eb1bab2d6972cc1d396ae5326217a9e5196f8d
ossama <ossama@gmail.com>
2024-11-30 02:19:50 -0600

    re styled the footer

--- my-app/src/components/footer.jsx
+++ my-app/src/components/footer.jsx
@@ -2,8 +2,10 @@
 const Footer = ()=>{
 
     return(
-        <footer className="p-5 bg-blue-50   absolute bottom-0 w-full">
+        <footer className="p-5 bg-blue-50   absolute bottom-0 w-full font-semibold text-center">
                 {/* to do */}
+                <p>&copy; 2024 Your Doctor on Demand</p>
+
         </footer>
     )
 }
