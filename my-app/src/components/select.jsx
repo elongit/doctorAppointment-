57529811242f47355e4cacfedb52cb7753e7a123
ossama <ossama@gmail.com>
2024-11-30 02:51:15 -0600

    re styled the select components

--- my-app/src/components/select.jsx
+++ my-app/src/components/select.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const Select = ({name , id , options , selectedOption})=>{
 
     return(
-        <select name={name} id={ id }  className="bg-slate-100 border-2 border-white hover:border-blue-600 p-3 outline-none rounded md:w-full w-fit">
+        <select name={name} id={ id }  className="bg-slate-100 border-2 border-white hover:border-blue-600 flex-shrink-0 basis-60 p-3 outline-none rounded md:w-full w-fit">
             <option selected>{selectedOption}</option>
             {
                 options.map((opt, i)=>
