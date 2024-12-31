9880090af0ef1ab1d04bba992c7a1082f8357bb2
ossama <ossama@gmail.com>
2024-12-31 12:54:39 -0600

    fixed some routing issue

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 
-// components
+// Components
 import Header from "./components/header";
-import TestimonialPage from "./pages/testimonialsPage";
+import Footer from "./components/footer";
 import Home from "./pages/home";
 import Login from "./pages/auth/Login/loginForm";
 import SignUp from "./pages/auth/signUp/signUpForm";
@@ -10,14 +10,11 @@ import ForgetPassword from "./pages/auth/forgetPass";
 import EmailCode from "./pages/auth/emailCode";
 import ResetPassword from "./pages/auth/resetPass";
 import Contact from "./pages/contact/contact";
-
+import TestimonialPage from "./pages/testimonialsPage";
 import DoctorList from "./pages/doctors/doctorsList";
-import NotFound from "./pages/404";
-
-
-import Footer from "./components/footer";
 import DoctorProfile from "./pages/doctors/doctorProfile";
 import TimeSlots from "./testing";
+import NotFound from "./pages/404";
 
 function App() {
   return (
@@ -29,47 +26,35 @@ function App() {
 
 const AppContent = () => {
   const { pathname } = useLocation();
-  const routes = [
-    { path: '/', component: <Home /> },
-    {path:'/timeslote', component : <TimeSlots/>},
-    { path: '/login', component: <Login /> },
-    { path: '/contact', component: <Contact /> },
-    { path: '/signUp', component: <SignUp /> },
-    { path: '/doctors', component: <DoctorList /> },
-    { path: '/*', component: <NotFound /> },
-    { path: '/forgetPass', component: <ForgetPassword /> },
-    { path: '/emailCode', component: <EmailCode /> },
-    { path: '/resetPass', component: <ResetPassword /> },
-    { path: '/TestimonialPage', component: <TestimonialPage /> },
 
-    { path: '/booking', component: <DoctorProfile /> },
-
-  ];
+  
+  const isSpecialPage = pathname === '/TestimonialPage' || pathname.startsWith('/booking/');
 
 
-  let header , footer ;
-
-  if (pathname === '/TestimonialPage' || pathname === '/booking' || pathname === '/doctorprofile' ) {
-    header =  null;
-    footer = null
-
-    
-  } else {
-    header = <Header />;
-    footer = <Footer/>;
-  }
+  const header = isSpecialPage ? null : <Header />;
+  const footer = isSpecialPage ? null : <Footer />;
 
   return (
     <>
       {header}
-     
-        <main className="lg:pt-0 pb-20">
-          <Routes>
-            {routes.map((route) => (
-              <Route key={route.path} path={route.path} element={route.component} />
-            ))}
-          </Routes>
-        </main>
+      
+      <main className="lg:pt-0 pb-20">
+        <Routes>
+          {/* Define all your routes here */}
+          <Route path="/" element={<Home />} />
+          <Route path="/timeslote" element={<TimeSlots />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/doctors" element={<DoctorList />} />
+          <Route path="/forgetPass" element={<ForgetPassword />} />
+          <Route path="/emailCode" element={<EmailCode />} />
+          <Route path="/resetPass" element={<ResetPassword />} />
+          <Route path="/TestimonialPage" element={<TestimonialPage />} />
+          <Route path="/booking/:id" element={<DoctorProfile />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </main>
       
       {footer}
     </>
